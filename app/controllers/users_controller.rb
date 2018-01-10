f408d9a61f9dfca219a8a290612f83b1a77a146a
Cassiano Mangini <cassiano.mangini@gmail.com>
2018-01-10 16:33:28 -0200

    #

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,14 @@
 class UsersController < ApplicationController
+
+  def new
+    new_list_user
+    @user = User.all
+  end
+
+  def new_list_user
+      url = 'https://randomuser.me/api/'
+      user_serialized = open(url).read
+      user = JSON.parse(user_serialized)
+
+  end
 end
