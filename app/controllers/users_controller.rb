b1cbc4175d53a612ae9c5f31de0f9764fa507a45
Cassiano Mangini <cassiano.mangini@gmail.com>
2018-01-11 14:41:23 -0200

    added a few steps

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,26 @@
 class UsersController < ApplicationController
 
-  def new
+
+  def index
+    User.delete_all
     new_list_user
-    @user = User.all
+    @users = User.all
   end
 
   def new_list_user
+    30.times do
       url = 'https://randomuser.me/api/'
-      user_serialized = open(url).read
-      user = JSON.parse(user_serialized)
+      response = HTTParty.get(url)
+      data = response.parsed_response
 
+      user.gender = data["results"][0]["gender"]
+      user.title = data["results"][0]["name"]["title"]
+      user.first_name = data["results"][0]["name"]["first"]
+      user.last_name = data["results"][0]["name"]["last"]
+      user.email = data["results"][0]["email"]
+      user.picture = data["results"][0]["picture"]["large"]
+      user = User.new
+      user.save
+    end
   end
 end
