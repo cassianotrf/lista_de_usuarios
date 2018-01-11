aa09a4d08b1a4c26063200e353b2ea387054ded6
Cassiano Mangini <cassiano.mangini@gmail.com>
2018-01-11 14:39:47 -0200

    added a few steps

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root to: 'home#home'
-
+  root to: 'pages#home'
+  resources :users , only: [:index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
