75fd1d59c2cf32c74351c1efaf7aea3ff1e546b4
Cassiano Mangini <cassiano.mangini@gmail.com>
2018-01-12 14:07:38 -0200

    #

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,10 +2,10 @@ class UsersController < ApplicationController
 
   def index
     #User.delete_all
-    #new_list_user
-    if params[:keywords].present?
-      @users = User.search(params[:keywords], fields: [:first_name, :gender,
-        :title, :last_name, :email, :nat, :seed], page: params[:page], per_page: 100)
+    new_list_user
+    #if params[:keywords].present?
+    #  @users = User.search(params[:keywords], fields: [:first_name, :gender,
+    #    :title, :last_name, :email, :nat, :seed], page: params[:page], per_page: 100)
     end
   end
 
